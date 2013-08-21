49c603ebc4c46c65eb3c21165b519995dd990c00
Travis Alexander <travisism@gmail.com>
2013-08-21 13:32:50 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Wink Micro-Router
+==============
+
+Simple micro router for pretty URLs while building web front ends that are framework agnostic.
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// Wink Micro-Router
+$path = array_values(array_filter(explode('/', $_SERVER['PATH_INFO'])));
+$controller = preg_replace('/[\s\W]+/','-', strtolower($path[0]));
+$controller = (empty($controller)) ? 'home' : $controller;
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Wink Micro-Router</title>
+</head>
+<body>
+	<div class="app-header">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/info">Info</a></li>
+		</ul>
+	</div>
+	<div class="app-content">
+		<?php
+			if(file_exists($controller . '.phtml')) {
+				require_once($controller . '.phtml');
+			} else {
+				require_once('404.phtml');
+			}
+		?>
+	</div>
+</body>
+</html>
